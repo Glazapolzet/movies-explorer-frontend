34552b53bff146e6cc63c79fc404133500bf8a3d
Glazapolzet <VypZlol@yandex.ru>
2023-08-17 22:54:59 +0300

    feat: file structure and tools

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
